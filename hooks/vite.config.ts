f218bc502140eb69ff87fef6f966b61c134bd936
youdame <112458620+youdame@users.noreply.github.com>
2025-04-30 19:09:46 +0900

    chore: 프리티어 설정
    Co-authored-by: seo_dev <jin123457@users.noreply.github.com>

--- hooks/vite.config.ts
+++ hooks/vite.config.ts
@@ -1,26 +1,26 @@
-import * as path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import dts from "vite-plugin-dts";
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/lib/index.ts"),
-      name: "index",
-      fileName: "index",
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'index',
+      fileName: 'index'
     },
     rollupOptions: {
-      external: ["react"],
+      external: ['react'],
       output: {
         globals: {
-          react: "React",
-        },
-      },
+          react: 'React'
+        }
+      }
     },
     commonjsOptions: {
-      esmExternals: ["react"],
-    },
+      esmExternals: ['react']
+    }
   },
-  plugins: [react(), dts()],
+  plugins: [react(), dts()]
 });
