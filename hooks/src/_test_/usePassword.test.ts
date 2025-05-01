a6a9bd76f29c4ab7e9f24c38d73515ff96e7b844
youdame <112458620+youdame@users.noreply.github.com>
2025-05-01 13:05:53 +0900

    test: usePassword 훅 테스트 코드 변경
    Co-authored-by: seo_dev <jin123457@users.noreply.github.com>

---
+++ hooks/src/_test_/usePassword.test.ts
@@ -0,0 +1,58 @@
+import { renderHook, act } from '@testing-library/react';
+import { ChangeEvent } from 'react';
+import usePassword from '../lib/password/usePassword';
+
+describe('usePassword 성공 케이스', () => {
+  it('모든 입력값이 없을 경우, 에러 메시지는 비어 있어야 한다.', () => {
+    const { result } = renderHook(() => usePassword());
+
+    act(() => {
+      result.current.handlePasswordChange({ target: { value: '' } } as ChangeEvent<HTMLInputElement>);
+    });
+
+    expect(result.current.errorState.errorMessage).toBe('');
+  });
+
+  it('모든 입력값이 유효할 경우, 에러 메시지는 비어 있어야 한다.', () => {
+    const { result } = renderHook(() => usePassword());
+
+    act(() => {
+      result.current.handlePasswordChange({ target: { value: '11' } } as ChangeEvent<HTMLInputElement>);
+    });
+
+    expect(result.current.errorState.errorMessage).toBe('');
+  });
+
+  it('입력값이 정확히 업데이트 되어야 한다.', () => {
+    const { result } = renderHook(() => usePassword());
+
+    act(() => {
+      result.current.handlePasswordChange({ target: { value: '11' } } as ChangeEvent<HTMLInputElement>);
+    });
+
+    expect(result.current.password).toBe('11');
+  });
+});
+
+describe('usePassword 실패 케이스', () => {
+  it('숫자가 아닌 문자를 입력하면 "숫자만 입력하세요." 에러가 반환된다', () => {
+    const { result } = renderHook(() => usePassword());
+
+    act(() => {
+      result.current.handlePasswordChange({ target: { value: '11d' } } as ChangeEvent<HTMLInputElement>);
+    });
+
+    expect(result.current.errorState.errorMessage).toBe('숫자만 입력하세요.');
+  });
+
+  it('3자리 숫자가 아니면 "3자리 숫자를 입력하세요." 에러가 반환된다', () => {
+    const { result } = renderHook(() => usePassword());
+
+    act(() => {
+      result.current.handlePasswordChange({ target: { value: '123' } } as ChangeEvent<HTMLInputElement>);
+      result.current.handlePasswordChange({ target: { value: '3' } } as ChangeEvent<HTMLInputElement>);
+    });
+
+    expect(result.current.errorState.errorMessage).toBe('2자리 숫자를 입력하세요.');
+  });
+});
