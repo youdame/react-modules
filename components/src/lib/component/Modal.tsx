99ca19ff9f70cd97463e9e4f5e157b9d4c8006c5
youdame <112458620+youdame@users.noreply.github.com>
2025-05-09 22:34:33 +0900

    refactor: 포탈 코드 걷어내기

--- components/src/lib/component/Modal.tsx
+++ components/src/lib/component/Modal.tsx
@@ -1,36 +1,18 @@
-import ModalPortal from '../ModalPortal';
 import useScrollBlock from '../hooks/useScrollBlock';
 import { ModalContext, useModalContext } from '../ModalContext';
 import { BackDrop, ModalWrapper, StyledFooter } from './Modal.styles';
 import { useClickAway } from '../hooks/useClickAway';
 import { ModalBackDropProps, ModalCloseButtonProps, ModalContentProps, ModalFooterProps, ModalMainProps, ModalTitleProps } from '../type/Modal.types';
 import useEscKeydown from '../hooks/useEscKeydown';
-import { useEffect, useRef } from 'react';
+
 import { createPortal } from 'react-dom';
 
 function ModalMain({ isOpen, size, onClose, position, children }: ModalMainProps) {
-  const containerRef = useRef<HTMLDivElement | null>(null);
-
   useEscKeydown(onClose);
   useScrollBlock(isOpen);
 
-  // create div for portal
-  if (!containerRef.current) {
-    containerRef.current = document.createElement('div');
-  }
-
-  // append to body
-  useEffect(() => {
-    const el = containerRef.current!;
-    document.body.appendChild(el);
-    return () => {
-      document.body.removeChild(el);
-    };
-  }, []);
-
   if (!isOpen) return null;
-  console.log(containerRef.current);
-  return createPortal(<ModalContext.Provider value={{ onClose, position, size }}>{children}</ModalContext.Provider>, containerRef.current);
+  return createPortal(<ModalContext.Provider value={{ onClose, position, size }}>{children}</ModalContext.Provider>, document.body);
 }
 
 function ModalBackDrop({ ...props }: ModalBackDropProps) {
