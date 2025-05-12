f63fb372708f87d1eb91e65ca021950a8eb4cfb6
youdame <112458620+youdame@users.noreply.github.com>
2025-05-12 13:23:02 +0900

    refactor: 불필요한 파일 제거

--- hooks/src/lib/constants/cardNumber.ts
+++
@@ -1,6 +0,0 @@
-export const initialCardNumberState = {
-  first: '',
-  second: '',
-  third: '',
-  fourth: ''
-};
--- hooks/src/lib/types/cardNumber.ts
+++
@@ -1,5 +0,0 @@
-import { initialCardNumberState } from '../constants/cardNumber';
-
-export type CardNumberStateType = typeof initialCardNumberState;
-
-export type CardNumberStateKey = keyof CardNumberStateType;
